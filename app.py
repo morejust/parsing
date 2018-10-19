3c7a7a4c669516506aab7340467a5ebbe050f3e1
Dannil <danokhlopkov@gmail.com>
2018-10-19 23:03:33 +0200

    add sources method

--- app.py
+++ app.py
@@ -14,29 +14,33 @@ def home():
 <p>Move your ass and cal the /parse method with 'url' param.</p>'''
 
 def parse(url):
-    a = Article(url, keep_article_html=True)
-    a.download()
-    a.parse()
-    a.nlp()
+    try:
+        a = Article(url, keep_article_html=True)
+        a.download()
+        a.parse()
+        a.nlp()
 
-    return  {
-        "author": ", ".join(a.authors),
-        "source": a.source_url[a.source_url.find("//") + 2:].split("/")[0],
-        "title": a.title,
-        "image": a.top_image,
-        "url": a.url,
-        "publishedAt": a.publish_date,
-        "html": a.article_html,
-        "text": a.text,
-        "summary": a.summary,
-        "keywords": a.keywords,
-    }
+        return  {
+            "author": ", ".join(a.authors),
+            "source": a.source_url[a.source_url.find("//") + 2:].split("/")[0],
+            "title": a.title,
+            "image": a.top_image,
+            "url": a.url,
+            "publishedAt": a.publish_date,
+            "html": a.article_html,
+            "text": a.text,
+            "summary": a.summary,
+            "keywords": a.keywords,
+        }
+    except Exception as e:
+        print(e)
+        return { 'error': True, 'description': "'%s' parsing went wrong with error: '%s'" % (url, e)}
 
 @app.route('/analyse', methods=['GET'])
 def analyse():
     query_parameters = request.args
     url = query_parameters.get('url')
-    result = { 'url': url, 'error': False}
+    result = { 'url': url, 'error': False }
 
     try:
         result['post'] = parse(url)
@@ -49,7 +53,26 @@ def analyse():
     except Exception as e:
         print(e)
         result["error"] = True
+        result["description"] = e
     return jsonify(result)
+
+@app.route('/sourceArticles', methods=['GET'])
+def source_articles():
+    query_parameters = request.args
+    source = query_parameters.get('source')
+    if source is None:
+        return jsonify({'error': True, 'description': 'No source provided'})
+    result = { 'source': source, 'error': False, 'acticles': [] }
+    try:
+        source_urls = ['url.com', 'url1.com', 'rul2.com']
+        result['acticles'] = [{ 'url': u } for u in source_urls]
+
+    except Exception as e:
+        print(e)
+        result["error"] = True
+        result["description"] = e
+    return jsonify(result)
+
 
 
 if __name__ == "__main__":
